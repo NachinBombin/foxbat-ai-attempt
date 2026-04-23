4184f24b401c2e1fc32c2bc0142cb65bab48e86c
NachinBombin <solidnachox@gmail.com>
2026-04-23 18:44:42 -0400

    fix: cl_init stripped to rotor-only, no LVS method calls

--- lua/entities/ent_a22foxbat/cl_init.lua
+++ lua/entities/ent_a22foxbat/cl_init.lua
@@ -1,36 +1,11 @@
 include( "shared.lua" )
 
--- rotor bone, always max RPM, blur disc bodygroup
 local PROP_BONE = 9
 local PROP_RPM  = 700
 
-local _rotorAcc = 0
-
 function ENT:OnFrame()
-	local ft = RealFrameTime()
-
-	-- rotor
-	_rotorAcc = _rotorAcc + PROP_RPM * ft
-	local rot = Angle( 0, 0, _rotorAcc )
+	self._rotorAcc = ( self._rotorAcc or 0 ) + PROP_RPM * RealFrameTime()
+	local rot = Angle( 0, 0, self._rotorAcc )
 	rot:Normalize()
 	self:ManipulateBoneAngles( PROP_BONE, rot )
-
-	-- control surfaces
-	local s   = self:GetSteer()
-	local fts = ft * 10
-	local tPitch = -s.y * 30
-	local tYaw   = -s.z * 20
-	local tRoll  = math.Clamp( -s.x * 60, -30, 30 )
-	self._smPitch = self._smPitch and self._smPitch + (tPitch - self._smPitch) * fts or 0
-	self._smYaw   = self._smYaw   and self._smYaw   + (tYaw   - self._smYaw)   * fts or 0
-	self._smRoll  = self._smRoll  and self._smRoll  + (tRoll  - self._smRoll)  * fts or 0
-	self:ManipulateBoneAngles( 3, Angle( 0,  self._smRoll,  0 ) )
-	self:ManipulateBoneAngles( 4, Angle( 0, -self._smRoll,  0 ) )
-	self:ManipulateBoneAngles( 6, Angle( 0, -self._smPitch, 0 ) )
-	self:ManipulateBoneAngles( 5, Angle( self._smYaw, 0, 0 ) )
-
-	-- landing gear
-	self._smGear = self._smGear and self._smGear + (30 * (1 - self:GetLandingGear()) - self._smGear) * ft * 8 or 0
-	self:ManipulateBoneAngles( 1, Angle( 0, 30 - self._smGear, 0 ) )
-	self:ManipulateBoneAngles( 2, Angle( 0, 30 - self._smGear, 0 ) )
 end
